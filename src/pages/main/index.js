95a14a68831590fec3deafe62bdd993812043c11
Rafael Cremasco Lacerda <rafaelcremasco@hotmail.com>
2021-11-08 13:25:00 -0300

    Instalando o Mapbox e listando imóveis no React Native

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,11 +1,82 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { StatusBar } from 'react-native';
+import MapboxGL from '@mapbox/react-native-mapbox-gl';
+import {
+    Container,
+    AnnotationContainer,
+    AnnotationText
+} from './styles';
 
-import { View } from 'react-native';
+MapboxGL.setAccessToken('<aqui_vai_seu_token>');
 
-// import styles from './styles';
+export default class Main extends Component {
+    static navigationOptions = {
+        header: null,
+    }
 
-const Main = () => (
-    <View />
-);
+    state = {
+        locations: [],
+    }
+
+    renderLocations = () => (
+        this.state.locations.map(location => (
+            <MapboxGL.PointAnnotation
+                id={location.id.toString()}
+                coordinate={[parseFloat(location.longitude), parseFloat(location.latitude)]}
+            >
+                <AnnotationContainer>
+                    <AnnotationText>{location.price}</AnnotationText>
+                </AnnotationContainer>
+                <MapboxGL.Callout title={location.title} />
+            </MapboxGL.PointAnnotation>
+        ))
+    )
+
+    async componentDidMount() {
+        try {
+            const response = await api.get('/properties', {
+                params: {
+                    latitude: -27.210768,
+                    longitude: -49.644018,
+                },
+            });
+
+            this.setState({ locations: response.data });
+        } catch (err) {
+            console.tron.log(err);
+        }
+    }
+
+    render() {
+        return (
+            <Container>
+                <StatusBar barStyle="light-content" />
+                <MapboxGL.MapView
+                    centerCoordinate={[-49.6446024, -27.2108001]}
+                    style={{ flex: 1 }}
+                    styleURL={MapboxGL.StyleURL.Dark}
+                >
+                    {this.renderLocations()}
+                </MapboxGL.MapView>
+            </Container>
+        );
+    }
+}
+
+
+Main.navigationOptions = {
+    header: null,
+};
+
+Main.propTypes = {
+    navigation: PropTypes.shape({
+        state: PropTypes.shape({
+            params: PropTypes.shape({
+                token: PropTypes.string,
+            }),
+        }),
+    }).isRequired,
+};
 
 export default Main;
